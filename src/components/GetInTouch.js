f0bdfa3658f34601f4f97a31adbacf9dc0f224ca
merhmood <mahmudsuberu9@gmail.com>
2021-06-26 04:54:43 +0100

    desktop layout finalize

--- src/components/GetInTouch.js
+++ src/components/GetInTouch.js
@@ -7,27 +7,27 @@ const GetInTouch = () => {
         <div className='get-in-touch'>
             <h6>GET IN TOUCH</h6>
             <div>
-                <img src="" alt="socialMedia" />
+                <img src="https://nationaleyecenter.s3.af-south-1.amazonaws.com/social+media/phone.png" alt="socialMedia" />
                 <p>08165906309, 09071666691</p>
             </div>
             <div>
-                <img src="" alt="socialMedia" />
+                <img src="https://nationaleyecenter.s3.af-south-1.amazonaws.com/social+media/gmail.png" alt="socialMedia" />
                 <p>info@eyecenter.gov.ng</p>
             </div>
             <div>
-                <img src="" alt="socialMedia" />
+                <img src="https://nationaleyecenter.s3.af-south-1.amazonaws.com/social+media/facebook.png" alt="socialMedia" />
                 <p>www.facebook.com/neckaduna</p>
             </div>
             <div>
-                <img src="" alt="socialMedia" />
+                <img src="https://nationaleyecenter.s3.af-south-1.amazonaws.com/social+media/instagram.png" alt="socialMedia" />
                 <p>www.instagram.com/neckaduna</p>
             </div>
             <div>
-                <img src="" alt="socialMedia" />
+                <img src="https://nationaleyecenter.s3.af-south-1.amazonaws.com/social+media/twitter.png" alt="socialMedia" />
                 <p>www.twitter.com/neckaduna</p>
             </div>
             <div>
-                <img src="" alt="socialMedia" />
+                <img src="https://nationaleyecenter.s3.af-south-1.amazonaws.com/social+media/real-estate.png" alt="socialMedia" />
                 <p>National Eye Centre Off Nnamdi Azikiwe By-Pass Mando PMB 2267 Kaduna.National Eye Centre Off Nnamdi Azikiwe By-Pass Mando PMB 2267 Kaduna.</p>
             </div>
         </div>
